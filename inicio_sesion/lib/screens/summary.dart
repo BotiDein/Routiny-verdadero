8ce25d56aae3b1305a9eb0eeeb44f9ae037cf800
reyben0413 <82120422+reyben0413@users.noreply.github.com>
2025-05-12 14:06:57 -0500

    cambios a mi cuenta
    se agrego la función de cambiar de contraseña, ya sea que se olvidara o se quiera cambiar dentro de la app, además de que las sugerencias se guardan en firestore

--- inicio_sesion/lib/screens/summary.dart
+++ inicio_sesion/lib/screens/summary.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'home_screen.dart'; // ahora menu.dart se llama home_screen.dart
 
 class ResumenPersonalScreen extends StatelessWidget {
   const ResumenPersonalScreen({super.key});
